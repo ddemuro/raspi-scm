a0edaa8fe7e6d64c3fdc9c7871b1f7d70afd0e5d
Derek Demuro <ddemuro@gmail.com>
2016-09-20 19:51:25 -0500

    Enable access to gii

--- raspberry-scm/protected/config/main.php
+++ raspberry-scm/protected/config/main.php
@@ -24,7 +24,7 @@ return array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'test',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('192.168.1.13','::1'),
+			'ipFilters'=>array('192.168.1.*','::1'),
 		),
 	),
 
