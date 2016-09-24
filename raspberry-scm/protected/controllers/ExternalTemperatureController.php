12e0738c6bed15d349acca503d666c3ae6583c73
Derek Demuro <ddemuro@gmail.com>
2016-09-24 01:37:39 -0500

    Fix view all permissions

--- raspberry-scm/protected/controllers/ExternalTemperatureController.php
+++ raspberry-scm/protected/controllers/ExternalTemperatureController.php
@@ -26,7 +26,7 @@ class ExternalTemperatureController extends Controller {
     public function accessRules() {
         return array(
             array('allow', // allow all users to perform 'index' and 'view' actions
-                'actions' => array('index', 'view'),
+                'actions' => array('index', 'view', 'viewall'),
                 'users' => array('*'),
             ),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
