b9394200c1f9341d28bb6137a608d95ee3c3c656
Derek Demuro <ddemuro@gmail.com>
2016-09-24 23:21:37 -0500

    Test len.

--- raspberry-scm/protected/components/TemperatureController.php
+++ raspberry-scm/protected/components/TemperatureController.php
@@ -5,96 +5,96 @@
  * All controller classes for this application should extend from this base class.
  */
 class TemperatureController extends CApplicationComponent {
-    
+
     /**
      * If we're running in debug, we want to log the input.
      * @param type $datapin
      * @param type $tempprog
      */
-    public function debug($msg){
-        if(YII_DEBUG === TRUE){
+    public function debug($msg) {
+        if (YII_DEBUG === TRUE) {
             Yii::log("Temperature controller: $msg", CLogger::LEVEL_ERROR, "info");
         }
     }
-    
+
     /**
      * Gets Humidity and Temperature as array on DHT sensors.
      * @param type $datapin
      * @return type
      */
-    public function getHumidityTemp($datapin, $extended){
+    public function getHumidityTemp($datapin, $extended) {
         $tempprog = Yii::app()->functions->yiiparam('external_sensor_program', NULL);
         $this->debug("DataPin: $datapin, Program: $tempprog");
-        if($tempprog == NULL){
+        if ($tempprog == NULL) {
             Yii::log('No temperature sensor configured, cannot sense externally...', CLogger::LEVEL_ERROR, "info");
             return NULL;
         }
         do {
             $res = Yii::app()->RootElevator->executeRoot("$tempprog -MJP $datapin $extended 2>&1", false);
-        } while(strcmp($res[0], "Error ") == 0);
+        } while (strcmp($res[0], "Error ") == 0);
         $this->debug("Humidity Temp Return: $res");
         str_replace(" ", "", $res);
         $respli = explode("%", $res);
-        return$respli;        
+        return$respli;
     }
-    
+
     /**
      * Get temperature from sensor like DHT11.
      * @param type $datapin
      * @return type
      */
-    public function getTemperature($datapin, $extended){
+    public function getTemperature($datapin, $extended) {
         $tempprog = Yii::app()->functions->yiiparam('external_sensor_program', NULL);
         $this->debug("DataPin: $datapin, Program: $tempprog");
-        if($tempprog == NULL){
+        if ($tempprog == NULL) {
             Yii::log('No temperature sensor configured, cannot sense externally...', CLogger::LEVEL_ERROR, "info");
             return NULL;
         }
         do {
             $res = Yii::app()->RootElevator->executeRoot("$tempprog -MJP $datapin $extended 2>&1", false);
-        } while(strcmp($res[0], "Error ") ==0);
+        } while (strcmp($res[0], "Error ") == 0);
         $this->debug("Temperature Return: $res");
         str_replace(" ", "", $res);
         $respli = explode("%", $res);
         return[1];
     }
-    
+
     /**
      * Get humidity from sensors like DHT11
      * @param type $datapin
      * @return type
      */
-    public function getHumidity($datapin, $extended){
+    public function getHumidity($datapin, $extended) {
         $tempprog = Yii::app()->functions->yiiparam('external_sensor_program', NULL);
         $this->debug("DataPin: $datapin, Program: $tempprog");
-        if($tempprog == NULL){
+        if ($tempprog == NULL) {
             Yii::log('No temperature sensor configured, cannot sense externally...', CLogger::LEVEL_ERROR, "info");
             return NULL;
         }
         do {
             $res = Yii::app()->RootElevator->executeRoot("$tempprog -MJP $datapin $extended 2>&1", false);
-        } while(strcmp($res[0], "Error ") ==0);
+        } while (strcmp($res[0], "Error ") == 0);
         $this->debug("Humidity Return: $res");
         str_replace(" ", "", $res);
         $respli = explode("%", $res);
         return[0];
     }
-    
+
     /**
      * Get humidity from sensors like DHT11
      * @param type $datapin
      * @return type
      */
-    public function getInternalCPUTemp(){
+    public function getInternalCPUTemp() {
         $tempprog = Yii::app()->functions->yiiparam('cpu_tmp', NULL);
         $this->debug("Program: $tempprog");
-        if($tempprog == NULL){
+        if ($tempprog == NULL) {
             Yii::log('No temperature sensor configured, cannot sense inernally...', CLogger::LEVEL_ERROR, "info");
             return NULL;
         }
         $res = Yii::app()->RootElevator->executeRoot("cat $tempprog", false);
         $this->debug("Internal Temperature Return: $res");
-        if(count($res) != 5){
+        if (strlen($res) <= 7 || strlen($res) >= 2) {
             return NULL;
         }
         $res = ($res / 1000);
@@ -106,10 +106,10 @@ class TemperatureController extends CApplicationComponent {
      * @param type $datapin
      * @return type
      */
-    public function getInternalGPUTemp(){
+    public function getInternalGPUTemp() {
         $tempprog = Yii::app()->functions->yiiparam('gpu_tmp', NULL);
         $this->debug("Program: $tempprog");
-        if($tempprog == NULL){
+        if ($tempprog == NULL) {
             Yii::log('No temperature sensor configured, cannot sense inernally...', CLogger::LEVEL_ERROR, "info");
             return NULL;
         }
