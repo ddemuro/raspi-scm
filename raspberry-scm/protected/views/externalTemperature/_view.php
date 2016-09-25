38d8b617282c5c8daa356bf477e4b3330e2ed786
Derek Demuro <ddemuro@gmail.com>
2016-09-24 19:06:47 -0500

    Fix view.

--- raspberry-scm/protected/views/externalTemperature/_view.php
+++ raspberry-scm/protected/views/externalTemperature/_view.php
@@ -3,50 +3,26 @@
 /* @var $data ExternalTemperature */
 ?>
 
-<?php 
-if (isset($multi) && $multi == true): ?>
-    <?php foreach($multi as $model): ?>
-    <div class="view">
+<?php if (isset($multi) && $multi == true): ?>
+    <?php foreach ($model as $m): ?>
+        <div class="view">
 
-        <b><?php echo CHtml::encode($model->getAttributeLabel('date')); ?>:</b>
-        <?php echo CHtml::link(CHtml::encode($model->date), array('view', 'id' => $model->date)); ?>
-        <br />
+            <b><?php echo CHtml::encode($m->getAttributeLabel('date')); ?>:</b>
+            <?php echo CHtml::link(CHtml::encode($m->date), array('view', 'id' => $m->date)); ?>
+            <br />
 
-        <b><?php echo CHtml::encode($model->getAttributeLabel('humidity')); ?>:</b>
-        <?php echo CHtml::encode($model->humidity); ?>
-        <br />
+            <b><?php echo CHtml::encode($m->getAttributeLabel('humidity')); ?>:</b>
+            <?php echo CHtml::encode($m->humidity); ?>
+            <br />
 
-        <b><?php echo CHtml::encode($model->getAttributeLabel('temperature')); ?>:</b>
-        <?php echo CHtml::encode($model->temperature); ?>
-        <br />
+            <b><?php echo CHtml::encode($m->getAttributeLabel('temperature')); ?>:</b>
+            <?php echo CHtml::encode($m->temperature); ?>
+            <br />
 
-        <b><?php echo CHtml::encode($model->getAttributeLabel('log')); ?>:</b>
-        <?php echo CHtml::encode($model->log); ?>
-        <br />
+            <b><?php echo CHtml::encode($m->getAttributeLabel('log')); ?>:</b>
+            <?php echo CHtml::encode($m->log); ?>
+            <br />
 
-    </div>
-
+        </div>
     <?php endforeach; ?>
-<?php else: ?>
-
-<div class="view">
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('date')); ?>:</b>
-    <?php echo CHtml::link(CHtml::encode($data->date), array('view', 'id' => $data->date)); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('humidity')); ?>:</b>
-    <?php echo CHtml::encode($data->humidity); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('temperature')); ?>:</b>
-    <?php echo CHtml::encode($data->temperature); ?>
-    <br />
-
-    <b><?php echo CHtml::encode($data->getAttributeLabel('log')); ?>:</b>
-    <?php echo CHtml::encode($data->log); ?>
-    <br />
-
-</div>
-
 <?php endif; ?>
