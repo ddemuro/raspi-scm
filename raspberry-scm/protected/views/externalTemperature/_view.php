f6fa2324d7fe29332eb6b3f9b1438856dbf86072
Derek Demuro <ddemuro@gmail.com>
2016-09-26 18:04:09 -0500

    Min fixes.

--- raspberry-scm/protected/views/externalTemperature/_view.php
+++ raspberry-scm/protected/views/externalTemperature/_view.php
@@ -25,4 +25,24 @@
 
         </div>
     <?php endforeach; ?>
+<?php else: ?>
+            <div class="view">
+
+            <b><?php echo CHtml::encode($model->getAttributeLabel('date')); ?>:</b>
+            <?php echo CHtml::link(CHtml::encode($model->date), array('view', 'id' => $m->date)); ?>
+            <br />
+
+            <b><?php echo CHtml::encode($model->getAttributeLabel('humidity')); ?>:</b>
+            <?php echo CHtml::encode($model->humidity); ?>
+            <br />
+
+            <b><?php echo CHtml::encode($model->getAttributeLabel('temperature')); ?>:</b>
+            <?php echo CHtml::encode($model->temperature); ?>
+            <br />
+
+            <b><?php echo CHtml::encode($model->getAttributeLabel('log')); ?>:</b>
+            <?php echo CHtml::encode($model->log); ?>
+            <br />
+
+        </div>
 <?php endif; ?>
