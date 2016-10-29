a695325764d1df05fe7f71233622d32ed40ca74a
Derek Demuro <ddemuro@gmail.com>
2016-10-29 13:30:17 -0500

    Fix view

--- raspberry-scm/protected/views/internalTemperature/_view.php
+++ raspberry-scm/protected/views/internalTemperature/_view.php
@@ -5,6 +5,7 @@
 
 <div class="view">
 
+    <?php if(isset($model)): ?>
     <b><?php echo CHtml::encode($model->getAttributeLabel('date')); ?>:</b>
     <?php echo CHtml::link(CHtml::encode($model->date), array('view', 'id' => $model->date)); ?>
     <br />
@@ -12,5 +13,5 @@
     <b><?php echo CHtml::encode($model->getAttributeLabel('temperature')); ?>:</b>
     <?php echo CHtml::encode($model->temperature); ?>
     <br />
-
+   <?php endif; ?>
 </div>
